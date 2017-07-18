da75c7484b6d89e508d0c000c7299525fb96c257
João Gouveia <joao.gouveia@concrete.com.br>
2017-07-18 19:33:54 -0300

    acertando a base do texto animado

--- AndroidAnimationsPart1/app/src/main/java/br/com/concrete/androidanimationspart1/fragment/property/ValueTextFragment.java
+++ AndroidAnimationsPart1/app/src/main/java/br/com/concrete/androidanimationspart1/fragment/property/ValueTextFragment.java
@@ -1,12 +1,20 @@
 package br.com.concrete.androidanimationspart1.fragment.property;
 
 
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.graphics.drawable.ArgbEvaluator;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Random;
 
 import br.com.concrete.androidanimationspart1.R;
 import br.com.concrete.androidanimationspart1.fragment.AnimationFragment;
@@ -17,6 +25,7 @@ import br.com.concrete.androidanimationspart1.fragment.AnimationFragment;
 public class ValueTextFragment extends Fragment implements AnimationFragment {
 
     View view;
+    private TextView text;
 
     public static ValueTextFragment newInstance() {
         return new ValueTextFragment();
@@ -30,13 +39,18 @@ public class ValueTextFragment extends Fragment implements AnimationFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View layout = inflater.inflate(R.layout.fragment_base, container, false);
+        View layout = inflater.inflate(R.layout.fragment_base_text, container, false);
         view = layout.findViewById(R.id.view);
+        text = (TextView) layout.findViewById(R.id.text);
         return layout;
     }
 
     @Override
     public void runAnimation() {
         Toast.makeText(getContext(), "run", Toast.LENGTH_SHORT).show();
+    }
+
+    private int getRandomNumber(){
+        return new Random().nextInt(1000);
     }
 }
